0ecc9dbeec26411245ad9bca1fca2f1c4a7230fe
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-18 15:43:46 +0530

    Char Issue

---
+++ print.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2;
+    float num3,num4;
+    char a,b;
+
+    /*printf("\n\nEnter any two Interger numbers : \n" );
+    
+    printf("\nNumber 1 - ");
+    scanf("%d",&num1);
+
+    printf("\nNumber 2 - ");
+    scanf("%d",&num2);
+
+    printf("\nEnter any two Decimal numbers : \n" );
+
+    printf("\nNumber 1 - ");
+    scanf("%f",&num3);
+
+    printf("\nNumber 2 - ");
+    scanf("%f",&num4);*/
+
+    printf("\nEnter any two names (Maximum 10 charecters):\n ");
+
+    printf("\nName 1 - ");
+    scanf("%c",&a);
+
+    printf("\nName 2 - ");
+    scanf("%c",&b);
+
+    printf("\nThanks for Visit");
+
+}
+        
