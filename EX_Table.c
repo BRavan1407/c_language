518564c0115d33e00f1b65bef8e7ab675c06d87b
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-19 22:25:34 +0530

    table

---
+++ EX_Table.c
@@ -0,0 +1,42 @@
+//Program for multiplication Table.
+
+#include<stdio.h>
+void main()
+{
+    int num,ans;
+
+    //5x1=5
+
+    printf("\n\nEnter table number : ");
+    scanf("%d",&num);
+
+    ans=num*1;
+    printf("\n\n%d x 1 = %d",num,ans);
+
+    ans=num*2;
+    printf("\n\n%d x 2 = %d",num,ans);
+
+    ans=num*3;
+    printf("\n\n%d x 3 = %d",num,ans);
+
+    ans=num*4;
+    printf("\n\n%d x 4 = %d",num,ans);
+
+    ans=num*5;
+    printf("\n\n%d x 5 = %d",num,ans);
+
+    ans=num*6;
+    printf("\n\n%d x 6 = %d",num,ans);
+
+    ans=num*7;
+    printf("\n\n%d x 7 = %d",num,ans);
+
+    ans=num*8;
+    printf("\n\n%d x 8 = %d",num,ans);
+
+    ans=num*9;
+    printf("\n\n%d x 9 = %d",num,ans);
+
+    ans=num*10;
+    printf("\n\n%d x 10 = %d\n\n",num,ans);
+}
