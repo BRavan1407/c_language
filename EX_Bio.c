d9c7e23992bb4aa3a319ef81d40eb40d3a32ade7
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-19 19:56:25 +0530

    Bio

---
+++ EX_Bio.c
@@ -0,0 +1,13 @@
+//Bio Data of pruthviRaj
+
+#include<stdio.h>
+void main()
+{
+    printf("\n\n----------------------------------------------------------------------\n\n");
+    printf("\n\nName : Upadhyay PruthviRaj Vijaybhai");
+    printf("\n\nDate Of Birth : 9th of June 1995");
+    printf("\n\nEducation : Bechlor Of Engineering with Electrical Branch");
+    printf("\n\nMobile Number : 9904959695");
+    printf("\n\nE-mail : upadhyaypruthviraj131@gmail.com\n\n");
+    printf("\n\n----------------------------------------------------------------------\n\n");
+}
