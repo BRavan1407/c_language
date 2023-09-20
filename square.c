cd61e74c18276a40fb2145a2d221e63c044ec11f
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-20 16:43:43 +0530

    square

---
+++ square.c
@@ -0,0 +1,14 @@
+//Program to find square of numbers.
+
+#include<stdio.h>
+void main()
+{
+    int num;
+
+    printf("Enter number to find square :");
+
+    printf("\n\nNumber - ");
+    scanf("%d",&num);
+
+    printf("\n\nThe Square of given number is %d \n\n",num*num);
+}
