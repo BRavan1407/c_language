98ef118c40384f74f06cc770b511b7825822f170
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-20 20:35:21 +0530

    renames

---
+++ EX_cube.c
@@ -0,0 +1,12 @@
+//Program to find volume of cube.
+
+#include<stdio.h>
+void main()
+{
+    int edge;
+    printf("Enter the value of edge : ");
+    scanf("%d",&edge);
+
+    printf("\nVolume of cube is %d .\n ",edge*edge*edge);
+
+}
