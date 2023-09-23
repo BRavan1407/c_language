7ca1e6cbd77a948a29ad1e11e0d091dc3ea9c811
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-23 08:15:00 +0530

    bmi

--- BMI.c
+++ BMI.c
@@ -1,4 +1,4 @@
-//Program to find BMI of human
+//Program to find BMI. condition:  https://www.cdc.gov/healthyweight/images/assessing/bmi-adult-fb-600x315.jpg?_=07167
 
 #include<stdio.h>
 void main()
@@ -7,10 +7,10 @@ void main()
 
     printf("\n\nEnter asked values to find your BMI\n\n");
 
-    printf("Please Enter your weight in kilogram with decimal :");
+    printf("Please Enter your weight in kilogram :");
     scanf("%f",&weight);
 
-    printf("\nPlease Enter your height in foot with decimal : ");
+    printf("\nPlease Enter your height in foot : ");
     scanf("%f",&height);
 
     // printf("\nVerify your given Details : \n");
@@ -21,5 +21,30 @@ void main()
     bmi=weight/(Height*Height);
 
     printf("\nYour BMI is %.2f",bmi);
+
+    if(bmi<18.5)
+    {
+        printf("\n\nYou are under weight.");
+    }
+    else if (bmi>18.5 && bmi<=25)
+    {
+        printf("\n\nYour weight is normal.");
+    }
+    else if (bmi>25 && bmi<30)
+    {
+        printf("\n\nYou are over weight.");
+    }
+    else if (bmi>=30 && bmi<35)
+    {
+        printf("\n\nYou are obese.");
+    }
+    else
+    {
+        printf("\n\nYou are extremly obese");
+    }
+    
+    
+    
+
 
 }
