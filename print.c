38c5f6182f5a9ed3d06a2a090dcea15505ea2366
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-23 09:03:19 +0530

    ok

--- print.c
+++ print.c
@@ -1,36 +1,37 @@
-//Print all types of variables
-#include<stdio.h>
+// Print all types of variables
+#include <stdio.h>
+#include <stdlib.h>
 void main()
 {
-    int num1,num2;
-    float num3,num4;
-    char char1,char2;
+    int num1, num2;
+    float num3, num4;
+    char char1, char2;
 
-    /*printf("\n\nEnter any two Interger numbers : \n" );
-    
-    printf("\nNumber 1 - ");
-    scanf("%d",&num1);
-
-    printf("\nNumber 2 - ");
-    scanf("%d",&num2);
-
-    printf("\nEnter any two Decimal numbers : \n" );
+    printf("\n\nEnter any two Interger numbers : \n");
 
     printf("\nNumber 1 - ");
-    scanf("%f",&num3);
+    scanf("%d", &num1);
 
     printf("\nNumber 2 - ");
-    scanf("%f",&num4);*/
+    scanf("%d", &num2);
+
+    printf("\nEnter any two Decimal numbers : \n");
+
+    printf("\nNumber 1 - ");
+    scanf("%f", &num3);
+
+    printf("\nNumber 2 - ");
+    scanf("%f", &num4);
 
     printf("\nEnter any two Charecters:  ");
 
+    fflush(stdin);
     printf("\nCharecter 1 - ");
-    scanf("%c",&char1);
-
+    scanf("%c", &char1);
+    
+    fflush(stdin);
     printf("\nCharecter 2 - ");
-    scanf("%c",&char2);
+    scanf("%c", &char2);
 
     printf("\nThanks for Visit");
-
 }
-        
