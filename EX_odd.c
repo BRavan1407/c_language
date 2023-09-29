54904f701cf84b0bd22dc801606b4e7d5b040c10
BRavan1407 <114295018+BRavan1407@users.noreply.github.com>
2023-09-29 20:11:16 +0530

    odd

---
+++ EX_odd.c
@@ -0,0 +1,16 @@
+// Program to print odd number series
+
+#include<stdio.h>
+void main()
+{
+    int num;
+
+    num=1;
+
+    while (num<=300)
+    {
+        printf(" %d ,",num);
+        num=num+2;
+    }
+    
+}
